57011ba31f86bf31ef6f58b696a25fe074651d3e
Zishan Ali <zishanali06@gmail.com>
2019-03-29 11:07:22 -0500

    done

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-let namestring: string = "Zishan Ali";
+var namestring = "Zishan Ali";
 namestring = namestring.toUpperCase();
 console.log("Name: " + namestring);
 console.log("Career: Full Stack Engineer");
 console.log("Description: I like Game Of Thrones");
 console.log("");
-console.log("My Interests:")
-let myInterests: string[] = ['Football', 'Basketball', 'Games', 'Watching Movies'];
-for(let i: number = 0; i < myInterests.length; i++){
-    console.log('* '+ myInterests[i]);
-};
-
-function displayPosition(companyName: string, jobTitle: string, description: string){
+console.log("My Interests:");
+var myInterests = ['Football', 'Basketball', 'Games', 'Watching Movies'];
+for (var i = 0; i < myInterests.length; i++) {
+    console.log('* ' + myInterests[i]);
+}
+;
+function displayPosition(companyName, jobTitle, description) {
     console.log(jobTitle + " at " + companyName + " - " + description);
 }
 console.log("");
@@ -20,14 +20,14 @@ displayPosition("ITA Leads", "Web Developer", "Handled all front end and backend
 displayPosition("FreeMoola", "Founder", "Website where you can earn gift cards for doing surveys and watching videos");
 console.log("");
 console.log("My Skills:");
-function displaySkill(skillName: string, isCool: boolean){
-    if(isCool == true){
+function displaySkill(skillName, isCool) {
+    if (isCool == true) {
         console.log("* BAM: " + skillName);
-    } else {
+    }
+    else {
         console.log("* " + skillName);
     }
 }
-
 displaySkill("Java", false);
 displaySkill("C#", true);
 displaySkill("Eating", false);
